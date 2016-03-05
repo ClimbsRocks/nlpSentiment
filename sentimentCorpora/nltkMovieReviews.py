8faecf244e4060e132928c431996cd96b101a466
ClimbsRocks <climbsbytes@gmail.com>
2016-03-05 14:18:15 -0800

    finishes using nltk movie reviews to train sentiment classifier

--- sentimentCorpora/nltkMovieReviews.py
+++ sentimentCorpora/nltkMovieReviews.py
@@ -4,6 +4,18 @@ import random
 from sklearn.feature_extraction import DictVectorizer
 
 
+dv = DictVectorizer(sparse=True)
+
+# this function takes in a document, and then returns a dictionary with a consistent set of keys for every document
+# this ensures that we will have consistent features for each document we process, which becomes critical once we start getting predictions on new documents we did not train on
+def extractFeatures(doc):
+    docWords = set(doc)
+    docFeatures = {}
+
+    for word in popularWords:
+        docFeatures[word] = word in docWords
+    return docFeatures
+
 def getFeatures(numWordsToUse):
     # create a reviews list that holds two pieces of informatin on each reivew:
         # the words of the review
@@ -31,19 +43,13 @@ def getFeatures(numWordsToUse):
     # grab the top several thousand words, ignoring the 100 most popular
     # grabbing more words leads to more accurate predictions, at the cost of both memory and compute time
     # ignoring the 100 most popular is an easy method for handling stop words that are specific to this dataset, rather than just the English language overall
+    global popularWords
     popularWords = list(allWords.keys())[100:numWordsToUse]
 
-
-    # this function takes in a document, and then returns a dictionary with a consistent set of keys for every document
-    # this ensures that we will have consistent features for each document we process, which becomes critical once we start getting predictions on new documents we did not train on
-    def extractFeatures(doc):
-        docWords = set(doc)
-        docFeatures = {}
-
-        for word in popularWords:
-            docFeatures[word] = word in docWords
-        return docFeatures
-
+    outputMap = {
+        'neg': '0',
+        'pos': '4'
+    }
 
     formattedReviews = []
     reviewsSentiment = []
@@ -51,14 +57,25 @@ def getFeatures(numWordsToUse):
     for review, category in reviews:
         reviewFeatures = extractFeatures(review)
         formattedReviews.append(reviewFeatures)
-        reviewsSentiment.append(str(category))
+        reviewsSentiment.append( { 'sentiment': outputMap[category] } )
 
 
     # right now we have a data structure roughly equivalent to a dense matrix, except each row is a dictionary
     # DictVectorizer performs two key functions for us:
         # 1. turns each row form a dictionary into a vector using consistent placing of keys into indexed positions within each vector
         # 2. returns sparse vectors, saving enormous amounts of memory which becomes very useful when training our models
-    dv = DictVectorizer(sparse=True)
     sparseFeatures = dv.fit_transform(formattedReviews)
 
     return sparseFeatures, reviewsSentiment
+
+
+def formatTestData(testTweets):
+    formattedTweets = []
+    for tweet in testTweets:
+        tweetFeatures = extractFeatures(tweet)
+        formattedTweets.append(tweetFeatures)
+
+
+    sparseFeatures = dv.transform(formattedTweets)
+
+    return sparseFeatures
