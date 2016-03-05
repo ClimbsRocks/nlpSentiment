2d849fac7c1d37456911813762a6b10e15c08cb1
ClimbsRocks <climbsbytes@gmail.com>
2016-03-04 17:40:31 -0800

    creates script to add their algorithm's predictions to the test dataset

---
+++ theirAlgorithm.py
@@ -0,0 +1,75 @@
+import csv
+
+def loadDataset(fileName):
+    with open(fileName, 'rU') as trainingInput:
+        # detect the "dialect" of this type of csv file
+        try:
+            dialect = csv.Sniffer().sniff(trainingInput.read(1024))
+        except:
+            # if we fail to detect the dialect, defautl to Microsoft Excel
+            dialect = 'excel'
+        trainingInput.seek(0)
+        trainingRows = csv.reader(trainingInput, dialect)
+
+        allTweets = []
+        allTweetSentiments = []
+        entireDataset = []
+        for row in trainingRows:
+            # csv only gives us an iterable, not the data itself
+            entireDataset.append(row)
+
+    return entireDataset
+
+
+# testingTweets, testingSentiment = loadAndProcessData.loadDataset('testdata.manual.2009.06.14.csv', 1)
+entireDataset = loadDataset('testdata.manual.updated.csv')
+
+emoticons = {
+    '4': [':)','(:',': )','( :','=)','(=','= )','( =',':D',': D',':p',': p'],
+    '0': [':(','):',': (',') :','=(',')=','= (',') =',':D',': D',':p',': p']
+}
+
+print entireDataset
+theirAlgosPredictions = []
+
+for row in entireDataset:
+    tweet = row[5]
+
+    # set default sentiment score to 2 (neutral)
+    sentimentScore = 2
+
+    # try to find a positive emoticon
+    for emoticon in emoticons['4']:
+        if emoticon in tweet:
+            sentimentScore = 4
+
+    # try to find a negative emoticon
+    for emoticon in emoticons['0']:
+        if emoticon in tweet:
+            # if we have already found a positive emoticon for this message, call it neutral
+            if sentimentScore == 4:
+                sentimentScore = 2
+            else:
+                sentimentScore = 0
+
+    # add their algorithms prediction to each row
+    row.insert(0, sentimentScore)
+    # theirAlgosPredictions.append(sentimentScore)
+
+print entireDataset
+
+correctScores = 0
+incorrectScores = 0
+
+with open('testdata.manual.their.algo.predictions.csv', 'wb+') as writeFile:
+    csvWriter = csv.writer(writeFile, dialect='excel')
+    for row in entireDataset:
+        if str(row[0]) == str(row[1]):
+            correctScores += 1
+        else:
+            incorrectScores += 1
+
+        csvWriter.writerow(row)
+
+print 'correctScores' + str(correctScores)
+print 'incorrectScores' + str(incorrectScores)
